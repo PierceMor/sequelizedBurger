d704dcb65bbcd7bc05c0d97666f229f02412c296
Pierce <piercemorrill91@gmail.com>
2018-05-08 17:21:41 -0500

    something

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,13 @@ var connection;
 if ( process.env.JAWSDB_URL){
     connection = mysql.connection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
+    var connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
 }
 
 connection.connect(function (error ){
